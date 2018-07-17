37c4a47598bb9d6b17e5c7035490aedbcf3d411f
IreneSalomo <irene.salomo@gmail.com>
2018-07-17 17:43:32 +1000

    WIP Showing the Past Moves

--- src/index.js
+++ src/index.js
@@ -91,6 +91,15 @@ function Square(props) {
       const current = history[history.length-1];
       const winner = calculateWinner(current.squares);
 
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      })
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -109,7 +118,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div className="status">{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
